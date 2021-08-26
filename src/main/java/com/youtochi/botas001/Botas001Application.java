0c8afd3326d374097d4160fe96ec319ef1082950
Don Carlos Zendejas <czende@hotmail.com>
2021-08-26 13:12:47 -0500

    Add files via upload
    create project to deploy

---
+++ src/main/java/com/youtochi/botas001/Botas001Application.java
@@ -0,0 +1,13 @@
+package com.youtochi.botas001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Botas001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Botas001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/youtochi/botas001/Botas001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtochi.botas001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Botas001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
