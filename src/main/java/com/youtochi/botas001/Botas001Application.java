6e67bdb01902c4f21407b3dfc73c73b3fb0e876f
Don Carlos Zendejas <czende@hotmail.com>
2021-08-26 13:20:12 -0500

    Update Botas001Application.java

--- src/main/java/com/youtochi/botas001/Botas001Application.java
+++ src/main/java/com/youtochi/botas001/Botas001Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Botas001Application {
 
 	public static void main(String[] args) {
+		System.out.println("arrancando ...4");
 		SpringApplication.run(Botas001Application.class, args);
+		System.out.println("arrancado ...4");
 	}
 
 }
