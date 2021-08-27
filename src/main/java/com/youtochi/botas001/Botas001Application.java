334c1bfe7c9e5a1876169f7837a4729d504b667b
Don Carlos Zendejas <czende@hotmail.com>
2021-08-27 12:16:32 -0500

    Update Botas001Application.java
    added annotation //added this line,as in the localhost intellij it restcontrollers work ok, but in heroku i was getting 404

--- src/main/java/com/youtochi/botas001/Botas001Application.java
+++ src/main/java/com/youtochi/botas001/Botas001Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.youtochi"}) //added this line,as in the localhost intellij it restcontrollers work ok, but in heroku i was getting 404
 public class Botas001Application {
 
 	public static void main(String[] args) {
