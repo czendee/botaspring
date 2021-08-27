6d548a081c4eb30ddee254f36875739853c08437
Don Carlos Zendejas <czende@hotmail.com>
2021-08-27 12:43:53 -0500

    Update Botas001Application.java
    added extends  line and override the configure method ,as in the localhost intellij it restcontrollers work ok, but in heroku i was getting 404

--- src/main/java/com/youtochi/botas001/Botas001Application.java
+++ src/main/java/com/youtochi/botas001/Botas001Application.java
@@ -2,16 +2,20 @@ package com.youtochi.botas001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.youtochi.botas001"}) //added this line,as in the localhost intellij it restcontrollers work ok, but in heroku i was getting 404
-public class Botas001Application {
+public class Botas001Application extends SpringBootServletInitializer { {
 
 	public static void main(String[] args) {
 		System.out.println("arrancando ...4");
 		SpringApplication.run(Botas001Application.class, args);
 		System.out.println("arrancado ...4");
 	}
+	
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	  System.out.println("configurando ...4");
+      return builder.sources(Botas001Application.class);
+  }	
 
 }
