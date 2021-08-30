8308b221ec4c8a474c41fa09ee25f07079254eba
Don Carlos Zendejas <czende@hotmail.com>
2021-08-30 10:55:54 -0500

    Update DbFuenteDatos.java
    aadd annotation document import

--- src/main/java/com/youtochi/botas001/model/DbFuenteDatos.java
+++ src/main/java/com/youtochi/botas001/model/DbFuenteDatos.java
@@ -1,6 +1,7 @@
 package com.youtochi.botas001.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document (collection="youtochidatasource")
 public class DbFuenteDatos {
