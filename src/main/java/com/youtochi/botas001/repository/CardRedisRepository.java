802065fdc4358ff655902256134a8d7566339232
Don Carlos Zendejas <czende@hotmail.com>
2021-08-30 19:13:36 -0500

    Update CardRedisRepository.java
    add import

--- src/main/java/com/youtochi/botas001/repository/CardRedisRepository.java
+++ src/main/java/com/youtochi/botas001/repository/CardRedisRepository.java
@@ -2,6 +2,7 @@ package com.youtochi.botas001.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import com.youtochi.botas001.model.CardRedis;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardRedisRepository extends CrudRepository<CardRedis, String> {}
