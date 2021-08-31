e83e13294287d6c02dd575455df44e6ec280cd81
Don Carlos Zendejas <czende@hotmail.com>
2021-08-30 19:35:43 -0500

    Update Botas001Application.java

--- src/main/java/com/youtochi/botas001/Botas001Application.java
+++ src/main/java/com/youtochi/botas001/Botas001Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.youtochi.botas001.api.controller.LibroRestController;
 //import org.springframework.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @SpringBootApplication
 @EnableAutoConfiguration
