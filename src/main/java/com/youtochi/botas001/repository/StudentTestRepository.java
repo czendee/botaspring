9d7e8dfde8558c827eac09a6a9f67c930af25570
Don Carlos Zendejas <czende@hotmail.com>
2021-09-01 12:51:13 -0500

    Update StudentTestRepository.java
    fix class name

--- src/main/java/com/youtochi/botas001/repository/StudentTestRepository.java
+++ src/main/java/com/youtochi/botas001/repository/StudentTestRepository.java
@@ -2,5 +2,5 @@ package com.youtochi.botas001.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import com.youtochi.botas001.model.StudentTest;
-public interface StudentTestRepository extends CrudRepository<Student, String> {
+public interface StudentTestRepository extends CrudRepository<StudentTest, String> {
 }
