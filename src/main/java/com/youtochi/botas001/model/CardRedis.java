57436072c654ea00c72ae5ad90ba24fbc50d9999
Don Carlos Zendejas <czende@hotmail.com>
2021-09-01 11:30:47 -0500

    Update CardRedis.java
    set the /in the name

--- src/main/java/com/youtochi/botas001/model/CardRedis.java
+++ src/main/java/com/youtochi/botas001/model/CardRedis.java
@@ -3,26 +3,27 @@ package com.youtochi.botas001.model;
 import java.io.Serializable;
 import org.springframework.data.redis.core.RedisHash;
 
-//@RedisHash("#scrumblr#-room:luna25-cards")
+@RedisHash("#scrumblr#-room:/luna25-cards")
 //@RedisHash("scrumblr-room:luna25-cards")
-@RedisHash("luna25-cards")
+//@RedisHash("luna25-cards")
+//@RedisHash("scrumblr-room")
 public class CardRedis implements Serializable {
  
     private String id;
     private String text;
-    private String color;
+    private String colour;
     private String x;
     private String y;
   
   public void setId(){this.id=id;}
   public void setText(){this.text=text;}
-  public void setcolor(){this.color=color;}
+  public void setColour(){this.colour=colour;}
   public void setX(){this.x =x;}
   public void setY(){this.y=y;}
 
   public String getId(){ return id;}
   public String getText(){ return text;}
-  public String getColor(){ return color;}
+  public String getColour(){ return colour;}
   public String getX(){ return x;}
   public String getY(){ return y;}
 
