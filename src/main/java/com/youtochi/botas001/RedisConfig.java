d521f011d1b33d227cb4c346919467966006168a
Don Carlos Zendejas <czende@hotmail.com>
2021-09-01 10:11:14 -0500

    Update RedisConfig.java
    add config options

--- src/main/java/com/youtochi/botas001/RedisConfig.java
+++ src/main/java/com/youtochi/botas001/RedisConfig.java
@@ -18,20 +18,88 @@ public class RedisConfig {
 
 	@Autowired
 	private Environment env;
+	
+
+
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
 		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+		jedisPoolConfig.setMaxTotal(10);
+		jedisPoolConfig.setMaxIdle(5);
+		jedisPoolConfig.setMinIdle(1);
+		jedisPoolConfig.setTestOnBorrow(true);
+		jedisPoolConfig.setTestOnReturn(true);
+		jedisPoolConfig.setTestWhileIdle(true);
+		
 		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
 		jedisConnectionFactory.setHostName(env.getProperty("redis.host").trim());
 		jedisConnectionFactory.setPort(Integer.parseInt(env.getProperty("redis.port").trim()));
 		jedisConnectionFactory.setPassword(env.getProperty("redis.password").trim());
 		jedisConnectionFactory.setDatabase(Integer.parseInt(env.getProperty("redis.database").trim()));
 		jedisConnectionFactory.setUsePool(true);	
+		
 		jedisConnectionFactory.setPoolConfig(jedisPoolConfig);
 		return jedisConnectionFactory;
 	}
 
+	
+/*	
+	 @Bean
+
+	    public JedisConnectionFactory jedisConnectionFactory() throws URISyntaxException {
+
+		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+		jedisPoolConfig.setMaxTotal(10);
+
+		jedisPoolConfig.setMaxIdle(5);
+
+		jedisPoolConfig.setMinIdle(1);
+
+		jedisPoolConfig.setTestOnBorrow(true);
+
+		jedisPoolConfig.setTestOnReturn(true);
+
+		jedisPoolConfig.setTestWhileIdle(true);
+
+		    
+		String envRedisUrl = System.getenv("REDIS_URL");
+
+		URI redisUri = new URI(envRedisUrl);
+
+
+
+		RedisStandaloneConfiguration hostConfig = new RedisStandaloneConfiguration();
+
+		//hostConfig.setPort(redisUri.getPort());
+
+		//hostConfig.setHostName(redisUri.getHost());
+
+		///hostConfig.setPassword(redisUri.getUserInfo().split(":", 2)[1]);
+		hostConfig.setHostName(env.getProperty("redis.host").trim());
+		hostConfig.setPort(Integer.parseInt(env.getProperty("redis.port").trim()));
+		hostConfig.setPassword(env.getProperty("redis.password").trim());    
+		hostConfig.setDatabase(Integer.parseInt(env.getProperty("redis.database").trim()));
+
+		JedisClientConfiguration.JedisClientConfigurationBuilder builder = JedisClientConfiguration.builder();
+
+		JedisClientConfiguration clientConfig = builder
+
+			.usePooling()
+
+			.poolConfig(jedisPoolConfig)
+
+			.build();
+
+		JedisConnectionFactory factory = new JedisConnectionFactory(hostConfig, clientConfig);
+
+		return factory;
+
+	    }
+*/
+
+	
+	
 	@Bean
 	public RedisTemplate<String, String> redisTemplate() {
 		RedisTemplate<String, String> redisTemplate = new StringRedisTemplate();
